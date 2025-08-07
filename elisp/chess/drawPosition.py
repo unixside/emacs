8acc901f2f23bad495e43baac6bc57180be98efc
unixside <leandro.nehemias@gmail.com>
2025-08-06 21:58:48 -0300

    Adding interactive funcionality
    New flag --interactive to set chess position interactively

--- elisp/chess/drawPosition.py
+++ elisp/chess/drawPosition.py
@@ -1,6 +1,9 @@
 import os, sys, re, argparse
 from chess import Board, InvalidMoveError, IllegalMoveError, AmbiguousMoveError
 from PIL import Image, ImageDraw
+from prompt_toolkit import prompt
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 board_png = "./board.png"
 pieces_png = {
@@ -18,18 +21,76 @@ pieces_png = {
     "k" : "img/black-king.png"
 }
 
+arguments = {
+    "-w": {
+        "alt": "--white",
+        "default": "#ffffff",
+        "type": str,
+        "help": "color for white box"
+    },
+    "-b": {
+        "alt": "--black",
+        "default": "#1f1f1f",
+        "type": str,
+        "help": "color for black box"
+    },
+    "-p": {
+        "alt": "--pixelsize",
+        "default": 1160,
+        "type": int,
+        "help": "size for png image on px"
+    },
+    "-o": {
+        "alt": "--output",
+        "default": "./board.png",
+        "type": str,
+        "help": "path of png image"
+    },
+    "-s": {
+        "alt": "--swap",
+        "default": False,
+        "action": "store_true",
+        "help": "swap orientation of board"
+    },
+    "-m": {
+        "alt": "--movements",
+        "default": None,
+        "type": str,
+        "help": "movements for apply to board"
+    },
+    "-i": {
+        "alt": "--interactive",
+        "default": False,
+        "action": 'store_true',
+        "help": "Enter interactive movements"
+    }
+}
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 for key, path in pieces_png.items():
     pieces_png[key] = os.path.join(script_dir, path)
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-w', '--white', default="#ffffff", type=str, help='color for white box')
-parser.add_argument('-b', '--black', default="#1f1f1f", type=str, help='color for black box')
-parser.add_argument('-p', '--pixelsize', default=1160, type=int, help='size for png image on px')
-parser.add_argument('-o', '--output', default="./board.png", type=str, help="path of png image")
-parser.add_argument('-s', '--swap', action='store_true', help='swap orientation of board')
-parser.add_argument('-m', '--movements', type=str, help='movements for apply to board')
+for short, params in arguments.items():
+
+    if "action" in params:
+        parser.add_argument(
+            short,
+            params["alt"],
+            default=params["default"],
+            action=params["action"],
+            help=params["help"]
+        )
+    else:
+        parser.add_argument(
+            short,
+            params["alt"],
+            default=params["default"],
+            type=params["type"],
+            help=params["help"]
+        )
 
 def isEven(n):
     return n % 2 == 0
@@ -97,31 +158,57 @@ def replaceMayusPieces(move):
     else:
         return move
 
-def main():
+def try_move(move, board):
+    error = None
+    if isMayus(move[0]):
+        move = replaceMayusPieces(move)
+    try:
+        board.push_san(move)
+    except IllegalMoveError as e:
+        print(f"Error: {e} is a illegal move.")
+        error = IllegalMoveError
+    except InvalidMoveError as e:
+        print(f"Error: {e} is syntactically invalid move.")
+        error = IllegalMoveError
+    except AmbiguousMoveError as e:
+        print(f"Error {e} is a ambiguous move.")
+        error = IllegalMoveError
+        
+    return error
 
+def main():
     args = parser.parse_args()    
     board = Board()
-    movements = args.movements
-    movements = re.findall("[A-Za-z0-8+\\-]{2,5}", movements)
+    movements = args.movements if args.movements else []
+    errors = [IllegalMoveError, InvalidMoveError, AmbiguousMoveError]
+    err = None
 
-    for move in movements:
-        if isMayus(move[0]):
-           move = replaceMayusPieces(move)
+    
 
-        try:
-           board.push_san(move)
-        except IllegalMoveError as e:
-            print(f"Error: {e} is a illegal move.")
-            exit(1)
-        except InvalidMoveError as e:
-            print(f"Error: {e} is syntactically invalid move.")
-            exit(1)
-        except AmbiguousMoveError as e:
-            print(f"Error {e} is a ambiguous move.")
-            exit(1)
-        
-    drawBoard(board.board_fen(), args.swap, args.pixelsize, args.white, args.black, args.output)
+    if movements != []:
+        movements = re.findall("[A-Za-z0-8+\\-]{2,5}", movements)
+        for move in movements:
+            err = try_move(move, board)
+
+            if err in errors and not args.interactive:           
+                exit(1)
+
+    if args.interactive:
+        while True:
+            user_input=prompt(
+                ">>> ",
+                history=FileHistory("./history"),
+                auto_suggest=AutoSuggestFromHistory())
+
+            if 'q' in user_input.split():
+                break
+
+
+            try_move(user_input, board)
+            drawBoard(board.board_fen(), args.swap, args.pixelsize, args.white, args.black, args.output)
+
     exit(0)
 
+    
 if __name__ == '__main__':
     main()
